74896117a4ebf62fd0b898470457e1e64587df97
allanlburns <allanlburns@yahoo.com>
2017-05-04 22:01:31 -0400

    Create home page and add links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-	root 'welcome#home'
-	get 'about', to: 'welcome#about'
+	root 'pages#home'
+	get 'about', to: 'pages#about'
 
 	# generates RESTful routes for resources (try rake routes with and without "resources")
 	resources :articles
