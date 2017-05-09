31a186fac69ef84d514e574e6e33bead3e00fcaa
allanlburns <allanlburns@yahoo.com>
2017-05-08 21:45:47 -0400

    Sign up users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
 	# generates RESTful routes for resources (try rake routes with and without "resources")
 	resources :articles
+
+	get 'signup', to: 'users#new'
+	post 'users', to: 'users#create'
 end
